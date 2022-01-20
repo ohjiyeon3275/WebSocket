35c74cc4737d23fcbc0cbd6b4fe941f8bdb3aeaa
ohjiyeon3275 <ojy940716@gmail.com>
2022-01-20 22:05:49 +0900

    websocket config

--- src/main/java/com/jiyeon/project/controller/MainController.java
+++ src/main/java/com/jiyeon/project/controller/MainController.java
@@ -1,52 +1,21 @@
 package com.jiyeon.project.controller;
 
-import com.jiyeon.project.domain.User;
-import com.jiyeon.project.repository.UserRepository;
-import com.jiyeon.project.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.jiyeon.project.dto.*;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-import java.util.Optional;
-
-
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class MainController {
 
-    @Autowired
-    private UserService userService;
+    @MessageMapping("/msg")
+    @SendTo("/sockets/msg")
+    public ResponseMessage getMessage(Message message) throws InterruptedException {
 
-    @Autowired
-    private UserRepository userRepository;
-// issue test
-    @GetMapping("/main")
-    public ModelAndView main(@RequestParam Long id){
+        Thread.sleep(1000);
+        return new ResponseMessage(HtmlUtils.htmlEscape(message.getMessageContents()));
 
-        List<User> userList = userService.findByUserId(id);
-        ModelAndView mv = new ModelAndView();
-
-        Optional<User> userOptional = userRepository.findByUserId(id);
-
-        System.out.println(userOptional.get().getName());
-        System.out.println("------1user ----name -----");
-
-        System.out.println(userOptional.get().getAge());
-
-        System.out.println("------1user ----age -----");
-
-        System.out.println(userList.get(0).getUserId());
-        System.out.println("------user ----id -----");
-
-        System.out.println(userList.get(0).getName());
-        System.out.println("------user ----name -----");
-
-        mv.setViewName("main.html");
-
-        return mv;
     }
+
 }
