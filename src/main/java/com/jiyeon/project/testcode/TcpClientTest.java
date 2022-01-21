74ef200f440f6bdf72bf8fea9b5c54eb8c5e5aea
ohjiyeon3275 <ojy940716@gmail.com>
2022-01-21 23:23:11 +0900

    socket with socket-class, buffer and i/o stream

---
+++ src/main/java/com/jiyeon/project/testcode/TcpClientTest.java
@@ -0,0 +1,49 @@
+package com.jiyeon.project.testcode;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TcpClientTest {
+    public static void main(String[] args) throws IOException {
+
+        InetAddress localHost = InetAddress.getLocalHost();
+
+        int serverPort = 5000;
+
+        Socket socket = new Socket(localHost, serverPort);
+
+        try {
+            BufferedWriter sender = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            BufferedReader message = new BufferedReader(new InputStreamReader(System.in));
+
+            while(true){
+                System.out.println("please input message >> ");
+
+                String sendMessage = message.readLine();
+
+                /**
+                 * Params:
+                 * str – A String
+                 * off – Offset from which to start writing characters
+                 * len – Number of characters to write
+                 */
+                sender.write(sendMessage, 0, sendMessage.length());
+                sender.newLine();
+                sender.flush();
+
+                System.out.println("completed! tcp port number >> " + serverPort);
+                String receivedMessage = receiver.readLine();
+                System.out.println("received msg is >> " + receivedMessage);
+
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
