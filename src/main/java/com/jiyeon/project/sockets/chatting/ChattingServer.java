4bb81de3f8097d84613b0d9af1393d9701e21e23
ohjiyeon3275 <ojy940716@gmail.com>
2022-01-28 18:34:44 +0900

    chatting server

---
+++ src/main/java/com/jiyeon/project/sockets/chatting/ChattingServer.java
@@ -0,0 +1,39 @@
+package com.jiyeon.project.sockets.chatting;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class ChattingServer {
+
+    public static ServerSocket serverSocket = null;
+    public static Socket socketClient = null;
+    public static Vector vectors = new Vector();
+
+    public ChattingServer(String[] args) {
+        int serverPort = 5200;
+        try{
+            serverSocket = new ServerSocket(serverPort);
+            System.out.println(">>> server is running >>> ");
+            while(true){
+                socketClient = serverSocket.accept();
+                VectorClient vectorClient = new VectorClient(socketClient);
+                vectors.addElement(vectorClient);
+
+                System.out.println(">>> current users >>> " + vectors.size());
+
+                vectorClient.start();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new ChattingServer(args);
+    }
+}
---
+++ src/main/java/com/jiyeon/project/sockets/chatting/VectorClient.java
@@ -0,0 +1,92 @@
+package com.jiyeon.project.sockets.chatting;
+
+import ch.qos.logback.core.net.server.Client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Vector;
+
+
+public class VectorClient extends Thread {
+
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
+    Socket socket;
+
+    public VectorClient(Socket socket){
+        this.socket = socket;
+        try{
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        String receiver;
+
+        while(true){
+            try{
+                receiver = inputStream.readUTF();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                quit(this);
+                return ;
+            }
+
+            int tmp = receiver.indexOf("Quit");
+            if(tmp >= 0 ){
+                String nickName = receiver.substring(0, tmp);
+                send(nickName + " leave the room");
+                quit(this);
+                return ;
+            }else{
+                send(receiver);
+            }
+        }
+    }
+
+    public void send(String receivers) {
+        Vector vector = ChattingServer.vectors;
+
+        try {
+            for (int i = 0; i < vector.size(); i++) {
+                VectorClient vectorClient = (VectorClient) vector.elementAt(i);
+                vectorClient.outputStream.writeUTF(receivers);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void quit(VectorClient clients){
+
+        Vector vector = ChattingServer.vectors;
+        Socket socketClient = ChattingServer.socketClient;
+        int tmp = vector.indexOf(clients);
+        if(tmp >= 0){
+            vector.removeElementAt(tmp);
+            try{
+                clients.inputStream.close();
+                clients.outputStream.close();
+                socketClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(">>> current user number is " + vector.size());
+
+
+        }else{
+            System.out.println(">>> there is no vector to quit(); ");
+        }
+
+
+    }
+
+}
